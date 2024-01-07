c6872257c5926cbf8ffecee2823ceaf1a647fa74
JuanDiegoBA <jbricenoa92@gmail.com>
2024-01-07 18:01:21 -0500

    Este es el primer commit del proyecto con una descripcion basica de los framework a usar

---
+++ insideti/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Este sera el header del proyecto insideTI</h1>
+      <p>Este sera un proyecto de estudio para desarrollar habilidades fullstack como programdores, incialmente con ReactJS y NextJS, el back sera con Java</p>
+    </>
+
+  )
+}
